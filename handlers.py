4016c35342a87a489560847ba05f7e87f2f6fcca
oooprya <oooprya@gmail.com>
2023-12-17 13:57:29 +0200

    first commit

---
+++ handlers.py
@@ -0,0 +1,17 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+
+router = Router()
+
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне команду /id")
+
+
+@router.message(Command("id"))
+async def message_handler(msg: Message):
+    await msg.answer(f"Твой ID: {msg.from_user.id}")
+
